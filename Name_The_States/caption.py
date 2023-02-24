e9b92d8abb2b0b8bc813ec72c3804b40035953c0
TokuyaTakahashi <tokuyatakahashi@gmail.com>
2023-02-23 21:50:44 -0500

    created csv creation for missed states at the end

--- Name_The_States/caption.py
+++ Name_The_States/caption.py
@@ -4,9 +4,10 @@ from turtle import Turtle
 class Caption(Turtle):
 
     def __init__(self):
+        super().__init__()
         self.hideturtle()
         self.pu()
 
     def write_state(self, state_name, position):
         self.goto(position)
-        self.write(f"{state_name}", align='center', font=('Courier', 10, 'normal'))
+        self.write(f"{state_name}", align='center', font=('Courier', 7, 'normal'))
--- Name_The_States/main.py
+++ Name_The_States/main.py
@@ -1,5 +1,7 @@
 import turtle
+import pandas
 from caption import Caption
+
 image = 'blank_states_img.gif'
 
 screen = turtle.Screen()
@@ -7,8 +9,22 @@ screen.title("US: Name The States")
 screen.setup(width=730, height=490)
 screen.addshape(image)
 turtle.shape(image)
-
 caption = Caption()
-answer_state = screen.textinput(title="Guess The State", prompt="What's another state's name?")
+data = pandas.read_csv('50_states.csv')
+state_arr = data.state.to_list()
+guessed_state = []
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == 'Exit':
+        break
+    if answer_state in state_arr:
+        state_x_pos = int(data[data['state'] == answer_state].x)
+        state_y_pos = int(data[data['state'] == answer_state].y)
+        caption.write_state(answer_state, (state_x_pos, state_y_pos))
+        state_arr.pop(state_arr.index(answer_state))
+        guessed_state.append(answer_state)
 
+states_to_learn = {'states missed': state_arr}
+pandas.DataFrame(states_to_learn).to_csv('states_to_learn.csv')
 screen.exitonclick()
