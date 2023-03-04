782d5345981515d0ccaa99ac60ea22b30994bc82
TokuyaTakahashi <tokuyatakahashi@gmail.com>
2023-03-03 23:50:55 -0500

    Saves words to learn to csv. basic fnc complete

--- Flash_Card_App/main.py
+++ Flash_Card_App/main.py
@@ -4,23 +4,37 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 CARD_BACKGROUND = '#91C2AF'
-data = pandas.read_csv('./data/1000 japanese frequency translations.csv')
-words_dict = data.to_dict(orient='records')
+try:
+    data = pandas.read_csv('./data/words_to_learn.csv')
+except FileNotFoundError:
+    data = pandas.read_csv('./data/1000 japanese frequency translations.csv')
+    words_dict = data.to_dict(orient='records')
+else:
+    words_dict = data.to_dict(orient='records')
 timer = 0
-word = random.choice(words_dict)
+word = {}
+
 #-------------------------FUNCTIONS-----------------------------#
 
 
 def new_word():
     global word
     word = random.choice(words_dict)
-    word_label.config(text=f"{word['Japanese']}")
+    set_word(word['Japanese'])
     countdown(3)
 
+
+def correct():
+    global word, words_dict
+    words_dict.remove(word)
+    print(len(words_dict))
+    data = pandas.DataFrame(words_dict)
+    data.to_csv('./data/words_to_learn.csv', index=False)
+    new_word()
+
+
 def countdown(count):
-    global timer
-    global word
-    print(count)
+    global timer, word
     if count == 0:
         screen.after_cancel(timer)
         translation = word['English']
@@ -30,10 +44,10 @@ def countdown(count):
     else:
         timer = screen.after(1000, countdown, count - 1)
 
-#NEEDS THIS FUNCTION TO MAKE THE LABELS BACK IN JAPAHESE
-def set_word():
-    lang_label.config(text='Japanese', bg='white')
-    word_label.config(text='', bg='white')
+
+def set_word(jpn_word):
+    lang_label.config(text='Japanese', bg='white', fg='black')
+    word_label.config(text=f'{jpn_word}', bg='white', fg='black')
     canvas.itemconfig(canvas_image, image=card_front)
 
 #----------------------------UI---------------------------------#
@@ -57,9 +71,9 @@ word_label.place(x=320, y=263)
 check_mark = PhotoImage(file='./images/right.png')
 x_mark = PhotoImage(file='./images/wrong.png')
 correct_button = Button(image=check_mark, width=100, height=100,
-                        highlightthickness=0, borderwidth=0, bg=BACKGROUND_COLOR, command=new_word)
+                        highlightthickness=0, borderwidth=0, bg=BACKGROUND_COLOR, command=correct)
 wrong_button = Button(image=x_mark, width=100, height=100,
-                      highlightthickness=0, borderwidth=0, bg=BACKGROUND_COLOR)
+                      highlightthickness=0, borderwidth=0, bg=BACKGROUND_COLOR, command=new_word)
 correct_button.grid(row=1, column=0)
 wrong_button.grid(row=1, column=1)
 screen.mainloop()
